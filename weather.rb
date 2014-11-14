a8d21150cad04b02777421525eb4991503bf8914
Tkwon123 <tkw21189@gmail.com>
2014-11-13 21:44:29 -0500

    updated event handling

--- weather.rb
+++ weather.rb
@@ -1,17 +1,6 @@
 require 'open-uri'
 require 'json'
 
-baseurl =  "http://api.openweathermap.org/data/2.5/weather?q="
-#search = ARGV[0].gsub("\s","")
-search = "Washington,DC"
-appid = "&APPID=aade92db2619aca3f567b1ac542128b5"
-
-
-fullurl = "#{baseurl}#{search}#{appid}"
-
-data = open(fullurl).read
-json = JSON.parse(data)
-
 #Weather API returns Kelvins so we'll have to convert these
 def KelvinToCelcius(kelvin)
 	return (kelvin - 273.15).round(2)
@@ -21,7 +10,31 @@ def KelvinToFahrenheit(kelvin)
 	return (kelvin * 9/5 - 459.67).round(2)
 end
 
-#
+if ARGV.empty?
+	puts "Find weather in the following format: '[City], [State]'"
+else
+
+search = Array.new
+
+ARGV.each do |argv|
+	search << argv
+end
+
+search = search.join
+
+baseurl =  "http://api.openweathermap.org/data/2.5/weather?q="
+appid = "&APPID=aade92db2619aca3f567b1ac542128b5"
+
+fullurl = "#{baseurl}#{search}#{appid}"
+
+data = open(fullurl).read
+json = JSON.parse(data)
+
+if json["cod"] = 404
+	abort("City not found. Please try again.")
+end
+
+#assigning variables
 tempc = KelvinToCelcius(json["main"]["temp"])
 highc = KelvinToCelcius(json["main"]["temp_min"])
 lowc = KelvinToCelcius(json["main"]["temp_max"])
@@ -31,6 +44,7 @@ highf = KelvinToFahrenheit(json["main"]["temp_min"])
 lowf = KelvinToFahrenheit(json["main"]["temp_max"])
 
 #Output to console
+puts "Forecast for #{search}"
 puts "Temperature (F°) #{tempf}° (hi: #{highf}/lo: #{lowf})"
 puts "Temperature (C°) #{tempc}° (hi: #{highc}/lo: #{lowc})"
 puts ""
@@ -38,6 +52,7 @@ puts "Description: #{json['weather'].first['description']}" #Narrative gets retu
 puts "Wind: #{json['wind']['speed']} mph"
 puts "Cloudiness: #{json['clouds']['all']}%"
 
+end
 #Example API call provided by http://openweathermap.org/
 =begin
 {"coord":{"lon":139,"lat":35},
